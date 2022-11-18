cda43dc0ef0ddaf38ef48bdbd79ccd9c911273f6
s-furlong <sfurlong@8thlight.com>
2022-11-18 01:37:03 -0500

    test coverage added for TTT logic

--- lib/ttt_logic.rb
+++ lib/ttt_logic.rb
@@ -8,31 +8,31 @@ WIN_COMBINATIONS = [
     [0, 4, 8],
     [2, 4, 6]
   ].freeze
-  
+
 class Tic
     def initialize(arr)
       @arr = arr
     end
   
-    def go
+    def go(arr)
       # if over?(arr) #added - need this to avoid infinite loop problem on first play test -- BUT see below at play -- I put this in the wrong place
       #  return #added
       # else # added
       puts 'Please enter 1-9:'
       z = gets.chomp.to_i # changed input to z and took out anything after .  eg .to_i. -- chomp not help
       index = z.to_i - 1
-      if index.between?(0, 8) && !(@arr[index] != ' ' && @arr[index] != '')
+      if index.between?(0, 8) && !(arr[index] != ' ' && arr[index] != '')
         token = which_won(@arr)
-        @arr[index] = token
+        arr[index] = token
         # return
       else
         go
       end
-      puts " #{@arr[0]} | #{@arr[1]} | #{@arr[2]} "
+      puts " #{arr[0]} | #{arr[1]} | #{arr[2]} "
       puts '-----------'
-      puts " #{@arr[3]} | #{@arr[4]} | #{@arr[5]} "
+      puts " #{arr[3]} | #{arr[4]} | #{arr[5]} "
       puts '-----------'
-      puts " #{@arr[6]} | #{@arr[7]} | #{@arr[8]} "
+      puts " #{arr[6]} | #{arr[7]} | #{arr[8]} "
       # end # added
     end
   
@@ -48,8 +48,8 @@ class Tic
       end
     end
   
-    def won?
-      arr_empty = @arr.all? do |space|
+    def won?(arr)
+      arr_empty = arr.all? do |space|
         space == ' '
       end
       return false if arr_empty == true
@@ -59,7 +59,7 @@ class Tic
       WIN_COMBINATIONS.each do |winning_array|
         new_array = [] # placement is very important.  Did not work where this was outside the WIN_COMBINATIONS each do
         winning_array.each do |index|
-          new_array << @arr[index]
+          new_array << arr[index]
         end
         if [%w[X X X], %w[O O O]].include?(new_array)
           return winning_array # NOTE: the return is telling the whole method to stop! Distinguish this from break!
@@ -75,9 +75,9 @@ class Tic
       arr_full == true
     end
   
-    def draw?
-      any_winner = won?
-      arr_full = full?(@arr)
+    def draw?(arr)
+      any_winner = won?(arr)
+      arr_full = full?(arr)
       if any_winner != false && arr_full == true # Don't feel great about this, but it's how I tested for false if game won -- because won? will either produce false or an array -- it won't produce true
         false
       else
@@ -85,38 +85,38 @@ class Tic
       end
     end
   
-    def over?
-      draw? || won?
+    def over?(arr)
+      draw?(arr) || won?(arr)
     end
   
     def winner(arr)
-      return unless won? != false
+      return unless won?(arr) != false
   
-      winny = won?
+      winny = won?(arr)
       arr[winny[0]]
     end
   
     def check_and_ending(arr)
-      if draw?
+      if draw?(arr)
         puts 'Cats Game!'
         nil
-      elsif won?
+      elsif won?(arr)
         puts "Congratulations #{winner(arr)}!"
         nil
       end
     end
   
-    def tac
-      if over? == false # this is what solved it.  The problem I was having was one the very first test.
+    def tac(arr)
+      if over?(arr) == false # this is what solved it.  The problem I was having was one the very first test.
         loop do # the testing there indicated it needed an "over" first.
-          go          # That would make almost all the other tests pass, but it had to go here.
+          go(arr)          # That would make almost all the other tests pass, but it had to go here.
           break if over?
         end
       end
-      if draw?
+      if draw?(arr)
         puts 'Cats Game!'
       else
-        puts "Congratulations #{winner(@arr)}!"
+        puts "Congratulations #{winner(arr)}!"
       end
       nil
     end
--- spec/lib/ttt_logic_spec.rb
+++ spec/lib/ttt_logic_spec.rb
@@ -1,7 +1,161 @@
 require 'ttt_logic'
 
-describe "legacy Code" do
-    it "is true" do
-        expect(true).to eq(false)
+describe "Tic Class" do
+    context "which_won?" do
+        it "will produce an X token if the board is blank" do
+            arr = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+            tic = Tic.new(arr)
+
+            token = tic.which_won(arr)
+
+            expect(token).to eq("X")
+        end
+
+        it "will produce an X token there is an even number of X and O on board" do
+            arr = ['X', 'O', 'X', 'O', ' ', ' ', ' ', ' ', ' ']
+            tic = Tic.new(arr)
+
+            token = tic.which_won(arr)
+
+            expect(token).to eq("X")
+        end
+
+        it "will produce an O token there is an odd number of X and O on board" do
+            arr = ['X', 'O', 'X', ' ', ' ', ' ', ' ', ' ', ' ']
+            tic = Tic.new(arr)
+
+            token = tic.which_won(arr)
+
+            expect(token).to eq("O")
+        end
+    end
+
+    context "won?" do
+        it "will return winning index positions if there are three connecting tokens" do
+            arr = ['X', 'X', 'X', 'O', 'X', 'O', 'X', 'O', 'O']
+            tic = Tic.new(arr)
+
+            win = tic.won?(arr)
+
+            expect(win).to eq([0, 1, 2])
+        end
+
+        it "will return false if there are no combinatiions" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            draw = tic.won?(arr)
+
+            expect(draw).to eq(false)
+        end
+    end
+
+    context "full?" do
+        it "will return true if the board is full" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            full = tic.full?(arr)
+
+            expect(full).to eq(true)
+        end
+
+        it "will return false if there is still blank space" do
+            arr = ['X', 'O', 'X', 'X', ' ', 'X', ' ', 'X', 'O']
+            tic = Tic.new(arr)
+
+            in_progress = tic.full?(arr)
+
+            expect(in_progress).to eq(false)
+        end
+    end
+
+    context "draw?" do
+        it "will return false if the board has winning combination" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'X', 'O', 'O']
+            tic = Tic.new(arr)
+
+            win = tic.draw?(arr)
+
+            expect(win).to eq(false)
+        end
+
+        it "will return true if the board has no winning combination" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            draw = tic.draw?(arr)
+
+            expect(draw).to eq(true)
+        end
+    end
+
+    context "over?" do
+        it "will return win combination if the game is won" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'X', 'O', 'O']
+            tic = Tic.new(arr)
+
+            win = tic.over?(arr)
+
+            expect(win).to eq([0, 3, 6])
+        end
+
+        it "will return true if the game is draw" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            win = tic.over?(arr)
+
+            expect(win).to eq(true)
+        end
+
+        it "will return false if the game is draw" do
+            arr = ['X', 'O', ' ', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            not_over = tic.over?(arr)
+
+            expect(not_over).to eq(false)
+        end
+    end
+
+    context "winner" do
+        it "will return nil if there is no winner" do
+            arr = ['X', 'O', ' ', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            no_winner = tic.winner(arr)
+
+            expect(no_winner).to eq(nil)
+        end
+
+        it "will return token of the winner" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'X', 'O', 'O']
+            tic = Tic.new(arr)
+
+            winner = tic.winner(arr)
+
+            expect(winner).to eq("X")
+        end 
+    end
+
+    context "check_and_ending" do
+        it "will return nil is a draw" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            no_winner = tic.check_and_ending(arr)
+
+            expect(no_winner).to eq(nil)
+        end
+
+        it "will return nil if there is a winner" do
+            arr = ['X', 'O', 'X', 'X', 'O', 'X', 'O', 'X', 'O']
+            tic = Tic.new(arr)
+
+            no_winner = tic.check_and_ending(arr)
+
+            expect(no_winner).to eq(nil)
+        end
     end
 end
