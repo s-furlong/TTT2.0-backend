4fbec6ebd1f1183e86a0238e49e39a570f692475
s-furlong <sfurlong@8thlight.com>
2022-11-21 16:05:30 -0500

    test coverage for valid POST request

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -16,8 +16,8 @@ ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   config.use_transactional_fixtures = false
-  config.include FactoryGirl::Syntax::Methods
-  config.include Devise::Test::ControllerHelpers, type: :controller
+  # config.include FactoryGirl::Syntax::Methods
+  # config.include Devise::Test::ControllerHelpers, type: :controller
   config.before(:suite) do
     DatabaseCleaner.clean_with(:truncation)
   end
