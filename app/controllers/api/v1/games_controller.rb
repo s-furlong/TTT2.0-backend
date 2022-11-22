2984541d49e99f76e568d5d6674a53f937066906
s-furlong <sfurlong@8thlight.com>
2022-11-21 22:58:24 -0500

    index in controller request returns correct response

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -5,71 +5,19 @@ module Api
   module V1
     class GamesController < ApplicationController
       before_action :set_game, only: %i[show update destroy]
-      
-      def move
-        render :json => {:name => "any name"}
-        #REQUEST:
-        #board = arr ARRAY
-        
-        #RESPONSE:
-        #token = ttt_logic.which_won?(arr) STRING X or O
-        #win = ttt_logic.win?(arr) BOOLEAN
-        #draw = ttt_logic.draw?(arr) BOOLEAN
-        #winner = ttt_logic.winner(arr) X or O
-      end
-      
-      
+
       # GET /games
       def index
-        # require 'pry'
-        # binding.pry
+
         board = params["board"]
         game = Tic.new(board)
 
         game_over = game.win?(board)
-        render :json => {:win => game_over, :token => "w", :draw => true, :winner => "X" }
-      end
+        current_player = game.which_won(board)
+        tie = game.draw?(board)
+        winner = game.winner(board)
 
-      # GET /games/1
-      def show
-        render json: @game
-      end
-
-      # POST /games
-      def create
-        @game = Game.new(game_params)
-
-        if @game.save
-          render json: @game, status: :created, location: @game
-        else
-          render json: @game.errors, status: :unprocessable_entity
-        end
-      end
-
-      # PATCH/PUT /games/1
-      def update
-        if @game.update(game_params)
-          render json: @game
-        else
-          render json: @game.errors, status: :unprocessable_entity
-        end
-      end
-
-      # DELETE /games/1
-      def destroy
-        @game.destroy
-      end
-
-      private
-
-      # Use callbacks to share common setup or constraints between actions.
-      def set_game
-        @game = Game.find(params[:id])
-      end
-
-      # Only allow a list of trusted parameters through.
-      def game_params
-        params.require(:game).permit(:title, :token)
+        render :json => {:win => game_over, :token => current_player, :draw => tie, :winner => winner }
       end
     end
   end
--- spec/controller/games_controller_spec.rb
+++ spec/controller/games_controller_spec.rb
@@ -44,7 +44,7 @@ RSpec.describe "/games", type: :request do
         expect(response).to have_http_status(:ok)
         expect(response.content_type).to match(a_string_including("application/json"))
         response_body = JSON.parse(response.body, symbolize_names: true)
-        expect(response_body).to eq({:win => false, :token => "w", :draw => true, :winner => "X" })
+        expect(response_body).to eq({:win => false, :token => "X", :draw => false, :winner => nil })
     end
   end
 
